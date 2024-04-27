74c0d6190f5e7f59406d5125234942ebe54adbc5
Loer <128202627+ArchangelLoer@users.noreply.github.com>
2024-04-27 09:50:23 -0300

    Create Alimento.java

---
+++ Alimento.java
@@ -0,0 +1,38 @@
+public class Alimento {
+    private String nome;
+    private String tipo;
+    private Double quantidade;
+    
+    // CT
+    public Alimento(String nome, String tipo, Double quantidade) {
+        this.nome = nome;
+        this.tipo = localizacao;
+        this.quantidade = quantidade;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+           
+    public double getQuantidade() {
+        return quantidade;
+    }
+    
+    public void setQuantidade(Double quantidade) {
+        this.tipoIndustria = tipoIndustria;
+    }
+     
+}
